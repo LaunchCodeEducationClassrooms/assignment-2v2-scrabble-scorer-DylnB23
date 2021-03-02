e995ff15563b2e73ae3614810e3ea6c9110deb9c
Dyln Brewer <dyln.brewer23@gmail.com>
2021-03-02 20:36:19 +0000

    The scrabble scoring

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -32,27 +32,94 @@ function oldScrabbleScorer(word) {
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
-function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+function initialPrompt(word) {
+  console.log("Let's play some scrabble!\n")
+  wordToScore = input.question("Enter a word to score: ");
+  
+  
 };
 
-let simpleScore;
+let simpleScore = function(word){
+ 
+  word = word.toUpperCase();
+	let simplePoints = 0;
+ for (let i = 0; i < word.length; i++){
+  simplePoints+= 1 
+ }
+return simplePoints
+};
 
-let vowelBonusScore;
+let vowelBonusScore = function(word){
+ 
+  word = word.toUpperCase();
+  let vowelScore = 0;
+  for (let i = 0; i < word.length; i++){
+    if (word[i] === 'A' || word[i] === 'E' || word[i] === 'I' || word[i] === 'O' || word[i] === 'U'){
+   vowelScore+= 3
+  } else {
+   vowelScore+= 1
+  }
+  }
+return vowelScore;
+};
 
-let scrabbleScore;
+let scrabbleScore = function(word){
+  word = word.toLowerCase();
+  let newScore = 0;
+  for (i = 0; i < word.length; i++) {
+    newScore += newPointStructure[word[i]];
+  }
+  return newScore;
+};
 
-const scoringAlgorithms = [];
+const scoringAlgorithms = [
+  {name: 'Simple Score',
+  description: 'Each letter is worth 1 point.',
+  scoringFunction: simpleScore},
+  {name: 'Bonus Vowels',
+  description: 'Vowels are 3pts.',
+  scoringFunction: vowelBonusScore},
+  {name: 'Scrabble',
+  description: 'The traditional scoring algorithim.',
+  scoringFunction: scrabbleScore}
+];
 
-function scorerPrompt() {}
+function scorerPrompt(word) {
+let number = input.question(`Select a scoring algorithm:\n\n
+0 - ${scoringAlgorithms[0].name}: ${scoringAlgorithms[0].description}
+1 - ${scoringAlgorithms[1].name}: ${scoringAlgorithms[1].description}
+2 - ${scoringAlgorithms[2].name}: ${scoringAlgorithms[2].description}
+Enter 0, 1, or 2: `)
 
-function transform() {};
+  if (number === '0'){
+  console.log (`Score for '${wordToScore}': ${scoringAlgorithms[0].scoringFunction(wordToScore)}`)
 
-let newPointStructure;
+  } else if (number === '1'){
+  console.log (`Score for '${wordToScore}': ${scoringAlgorithms[1].scoringFunction(wordToScore)}`)
+
+  } else if (number === '2'){
+  console.log (`Score for '${wordToScore}': ${scoringAlgorithms[2].scoringFunction(wordToScore)}`)
+
+  }
+}
+
+function transform(oldPointStructure) {
+   let newPointStructure = {};
+   
+   for (pointValue in oldPointStructure) {
+    for (let i = 0; i < oldPointStructure.length; i++) {
+      newPointStructure[oldPointStructure[pointValue][i].toLowerCase()] = Number(pointValue);
+    }
+  }
+  return newPointStructure
+};
+
+let newPointStructure = transform(oldPointStructure)
+newPointStructure[''] = 0;
 
 function runProgram() {
-   initialPrompt();
-   
+  initialPrompt();
+  scorerPrompt();
 }
 
 // Don't write any code below this line //
